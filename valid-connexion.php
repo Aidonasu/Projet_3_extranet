60764c2db814f9315cb0a97324f83bc7d4f37689
Aidonas <manu_07130@hotmail.com>
2019-11-27 15:05:35 +0100

    Correction page de connexion

--- valid-connexion.php
+++ valid-connexion.php
@@ -7,12 +7,16 @@
    /* connection à la base de données */
    $pdo = new PDO('mysql:host=localhost;dbname=extranet-gbaf', 'root', 'f9m2zlri');
    $pdo->exec('SET NAMES UTF8');
-   $query = $pdo->prepare("SELECT * FROM account where username=? and password=?");
-   $query->execute([$login,$password]);
+   $query = $pdo->prepare("SELECT * FROM account where username=?");
+   $query->execute([$login]);
 
 /*On vérifie si les informations données sont corrects, si oui connexion on profil admin*/
    if($results = $query->fetch(PDO::FETCH_ASSOC)){
-     if($results['id_user']){
+
+     // Comparaison du pass envoyé via le formulaire avec la base
+     $isPasswordCorrect = password_verify($_POST['password'], $results['password']);
+
+     if($isPasswordCorrect){
        sleep(1);
        $_SESSION['prenom'] = $results['prenom'];
        $_SESSION['nom'] = $results['nom'];
