c048bde834423e8b7d28a7ee9ba4081bcdc288c5
Aidonas <manu_07130@hotmail.com>
2019-11-27 16:12:09 +0100

    Mise à jour

---
+++ login/forgot_password.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Projet3</title>
+    <link rel="stylesheet" href="../fontawesome5/web-fonts-with-css/css/fontawesome-all.min.css">
+    <link rel="stylesheet" href="../css/normalize.css">
+    <link rel="stylesheet" href="../css/style.css">
+  </head>
+  <body>
+    <header>
+      <img src="../logo/GBAF.png" alt="logo1">
+    </header>
+    <main>
+      <form class="" action="valid-connexion.php" method="post">
+      <fieldset class="login">
+         <legend>Récupération par nom d'utilisateur</legend>
+         <div class="login-connexion">
+           <p>
+              <label for="user">UserName :</label>
+              <input type="text" name="user">
+           </p>
+         </div>
+         <div class="center">
+           <input type="submit" name="recherche" value="Rechercher">
+         </div>
+      </fieldset>
+   </form>
+    </main>
+    <footer>
+      <ul>
+        <li>Mentions légales</li>
+        <li>Contact</li>
+      </ul>
+    </footer>
+  </body>
+</html>
