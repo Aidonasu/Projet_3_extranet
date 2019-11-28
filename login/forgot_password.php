9e795fd9a96e64307462551f80e2c01917fa81a0
Aidonas <manu_07130@hotmail.com>
2019-11-28 21:34:56 +0100

    Ajout Page forgot password

--- login/forgot_password.php
+++ login/forgot_password.php
@@ -18,14 +18,29 @@
         <?php if (isset($_GET['error'])): ?>
         <p>Mauvais UserName !</p>
         <?php endif; ?>
-      <fieldset class="login">
+      <fieldset class="forgot">
          <legend>Récupération par nom d'utilisateur</legend>
-         <div class="login-connexion">
            <p>
-              <label for="user">UserName :</label>
-              <input type="text" name="user">
+              <label for="user">UserName</label>
+              <input class="form-input" type="text" name="user" required>
            </p>
-         </div>
+           <p>
+               <label for="question">Question secrète :</label>
+               <select class="form-input form-select" name="question" required>
+                 <option value="" selected>--- Selectionner une question ---</option>
+                 <option value="Quel est le nom de mon premier animal domestique ?">Quel est le nom de mon premier animal domestique ?
+                 <option value="Quel est le nom du pays que j’aimerais le plus visiter ?">Quel est le nom du pays que j’aimerais le plus visiter ?
+                 <option value="Quel est le nom du personnage historique que j’admire le plus ?">Quel est le nom du personnage historique que j’admire le plus ?
+                 <option value="Quelle est la  marque du premier véhicule que j’ai conduit ?">Quelle est la  marque du premier véhicule que j’ai conduit ?
+                 <option value ="Quelle est votre couleur préférée ?">Quelle est votre couleur préférée ?</option>
+                 <option value ="Quelle est votre équipe sportive favorite ?">Quelle est votre équipe sportive favorite ?</option>
+                 <option value ="Quel était le métier de votre grand-père ?">Quel était le métier de votre grand-père ?</option>
+               </select>
+           </p>
+           <p>
+             <label for="reponse">Réponse</label>
+             <input class="form-input" type="text" name="reponse" required>
+           </p>
          <div class="center">
            <input type="submit" name="recherche" value="Rechercher">
          </div>
