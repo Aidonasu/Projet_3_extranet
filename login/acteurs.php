be88f23166db5280ec53329589900798f8079d9b
Aidonas <manu_07130@hotmail.com>
2019-11-29 11:02:28 +0100

    Ajout vu commentaires

--- login/acteurs.php
+++ login/acteurs.php
@@ -19,7 +19,7 @@
   </head>
   <body>
     <header>
-      <img src="../logo/GBAF.png" alt="logo1">
+      <a href="index.php"><img src="../logo/GBAF.png" alt="logo1"></a>
 			<p>
 				<i class="fas fa-user"></i> <?=$_SESSION['nom']?> <?=$_SESSION['prenom'];?>
 				<br><br>
@@ -43,12 +43,28 @@
 				 <fieldset>
 				 	<legend>Commentaires</legend>
 					<div class="comments">
-						<a href="#">Nouveau commentaire</a>
+						<a href="new_comment_acteurs.php?acteurs=<?=$id;?>">Nouveau commentaire</a>
+						<input type="hidden" name="acteurs" value="<?=$id;?>">
 						<ul class="hand">
 							<li><i class="fas fa-thumbs-up"></i></li>
 							<li><i class="fas fa-thumbs-down"></i></li>
 						</ul>
 					</div>
+
+						<?php
+						require('../modele/pdoConnect.php');
+	          $query = $pdo->prepare('SELECT post,date_add FROM post where id_acteur = ?');
+	          $query->execute([$id]);
+	          $results = $query->fetchall(PDO::FETCH_ASSOC);
+						foreach ($results as $value) {
+							$date = substr($value['date_add'],0,10);
+	            $datefr = explode('-', $date);
+	        ?>
+					<div class="new_comment">
+						<p><?=$_SESSION['prenom'];?> le <?="$datefr[2]-$datefr[1]-$datefr[0]"?></p>
+						<p><?=$value['post'];?></p>
+					</div>
+				<?php } ?>
 				 </fieldset>
 				 <a href="index.php">Retour</a>
     </main>
