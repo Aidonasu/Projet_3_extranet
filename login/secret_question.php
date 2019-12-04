e71b7d96fba080df094674a4b1514d285aacc52d
Aidonas <manu_07130@hotmail.com>
2019-12-04 23:40:07 +0100

    Mise à jour page secret_question

--- login/secret_question.php
+++ login/secret_question.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Projet3</title>
     <link rel="stylesheet" href="../fontawesome5/web-fonts-with-css/css/fontawesome-all.min.css">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
     <link rel="stylesheet" href="../css/normalize.css">
     <link rel="stylesheet" href="../css/style.css">
   </head>
@@ -15,34 +16,34 @@
     </header>
     <main>
       <form class="" action="valid_secret_question.php" method="post">
-      <fieldset class="forgot">
-         <legend>Question secrète</legend>
-         <p>Merci de bien reprendre votre question secrète et votre réponse, afin de créer un nouveau mot de passe.</p>
-
-           <p>
-               <label for="question">Question secrète :</label>
-               <select class="form-input form-select" name="question" required>
-                 <option value="" selected>--- Selectionner une question ---</option>
-                 <option value="Quel est le nom de mon premier animal domestique ?">Quel est le nom de mon premier animal domestique ?
-                 <option value="Quel est le nom du pays que j’aimerais le plus visiter ?">Quel est le nom du pays que j’aimerais le plus visiter ?
-                 <option value="Quel est le nom du personnage historique que j’admire le plus ?">Quel est le nom du personnage historique que j’admire le plus ?
-                 <option value="Quelle est la  marque du premier véhicule que j’ai conduit ?">Quelle est la  marque du premier véhicule que j’ai conduit ?
-                 <option value ="Quelle est votre couleur préférée ?">Quelle est votre couleur préférée ?</option>
-                 <option value ="Quelle est votre équipe sportive favorite ?">Quelle est votre équipe sportive favorite ?</option>
-                 <option value ="Quel était le métier de votre grand-père ?">Quel était le métier de votre grand-père ?</option>
-               </select>
-           </p>
-           <p>
-             <label for="reponse">Réponse</label>
-             <input class="form-input" type="text" name="reponse" required>
-           </p>
-           <ul class="account">
-             <li><input type="submit" name="recherche" value="Rechercher" ></li>
-             <li><a href="../index.php" class="button button-cancel">Annuler</a></li>
-           </ul>
-      </fieldset>
-
-   </form>
+        <div class="card border-primary mb-3">
+          <div class="card-header">Question secrète</div>
+          <div class="card-body">
+            <div class="form-input form-group">
+              <label for="question">Question secrète</label>
+              <select class="form-input form-select form-control" name="question" required>
+                <option value="" selected>--- Selectionner une question ---</option>
+                <option value="Quel est le nom de mon premier animal domestique ?">Quel est le nom de mon premier animal domestique ?
+                <option value="Quel est le nom du pays que j’aimerais le plus visiter ?">Quel est le nom du pays que j’aimerais le plus visiter ?
+                <option value="Quel est le nom du personnage historique que j’admire le plus ?">Quel est le nom du personnage historique que j’admire le plus ?
+                <option value="Quelle est la  marque du premier véhicule que j’ai conduit ?">Quelle est la  marque du premier véhicule que j’ai conduit ?
+                <option value ="Quelle est votre couleur préférée ?">Quelle est votre couleur préférée ?</option>
+                <option value ="Quelle est votre équipe sportive favorite ?">Quelle est votre équipe sportive favorite ?</option>
+                <option value ="Quel était le métier de votre grand-père ?">Quel était le métier de votre grand-père ?</option>
+              </select>
+            </div>
+            <p class="card-text">
+              <label for="reponse">Réponse</label>
+              <input class="form-input form-control" type="text" name="reponse" required>
+            </p>
+            <ul class="account center">
+              <li><input type="submit" class="btn btn-primary" value="Rechercher"></li>
+              <li><a href="../index.php" class="btn btn-link">Annuler</a></li>
+            </ul>
+          </div>
+        </div>
+      </form>
+      </form>
     </main>
     <footer>
       <ul>
