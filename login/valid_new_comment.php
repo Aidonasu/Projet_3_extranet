6f39b852c67adfaa845322c90fbfbd1ed7b45ac7
Aidonas <manu_07130@hotmail.com>
2019-12-04 23:42:11 +0100

    Mise à jour valid - 1 commentaire

--- login/valid_new_comment.php
+++ login/valid_new_comment.php
@@ -9,11 +9,22 @@ $new_comment = htmlspecialchars($_POST['new_comment']);
 require_once '../config-pdo.php';
 require '../modele/pdoConnect.php';  /* connection à la base de données */
 $pdo->exec('SET NAMES UTF8');
-$query = $pdo->prepare("INSERT INTO post (id_user, id_acteur, date_add, post)
-VALUES(?,?,now(),?)");
 
-$query->execute([$user,$acteurs,$new_comment]);
+$comment = $pdo->prepare("SELECT id_user,id_acteur FROM post where id_user=? and id_acteur=?");
+$comment->execute([$user,$acteurs]);
+$results = $comment->fetch(PDO::FETCH_ASSOC);
 
-header("Location:acteurs.php?acteurs=$acteurs");
-exit();
+if ($results['id_user']) {
+  header("Location:acteurs.php?acteurs=$acteurs&error=1");
+}
+else {
+  $query = $pdo->prepare("INSERT INTO post (id_user, id_acteur, date_add, post)
+  VALUES(?,?,now(),?)");
+
+  $query->execute([$user,$acteurs,$new_comment]);
+
+  header("Location:acteurs.php?acteurs=$acteurs");
+  exit();
+}
+
 ?>
