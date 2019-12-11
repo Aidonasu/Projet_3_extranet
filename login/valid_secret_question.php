7abf558bdf9f91b5649ce3eee68d818c06333606
Emmanuel Rodrigues <48475342+Aidonasu@users.noreply.github.com>
2019-12-12 00:07:00 +0100

    Correction - ajout error mauvaise question/reponse

--- login/valid_secret_question.php
+++ login/valid_secret_question.php
@@ -2,24 +2,28 @@
 
    $reponse = htmlspecialchars($_POST['reponse']);
    $question = htmlspecialchars($_POST['question']);
+   $iduser = $_POST['iduser'];
 
    /* connection à la base de données */
    require_once '../config-pdo.php';
    require '../modele/pdoConnect.php';
    $pdo->exec('SET NAMES UTF8');
-   $query = $pdo->prepare("SELECT id_user,question,reponse FROM account where question=? and reponse=?");
-   $query->execute([$question,$reponse]);
+   $query = $pdo->prepare("SELECT id_user,question,reponse FROM account where id_user=? and question=? and reponse=?");
+   $query->execute([$iduser,$question,$reponse]);
 
 /*On vérifie si les informations données sont corrects, si oui connexion on profil admin*/
    if($results = $query->fetch(PDO::FETCH_ASSOC)){
      // Comparaison du pass envoyé via le formulaire avec la base
      //$isPasswordCorrect = password_verify($_POST['password'], $results['password']);
 
-     if($question = $results['question'] || $reponse = $results['reponse']){
+     if($question = $results['question'] && $reponse = $results['reponse']){
        sleep(1);
-       $id = $results['id_user'];
-      header("Location:change_password.php?id_user=$id");
+      header("Location:change_password.php?id_user=$iduser");
        exit();
      }
+   }
+   else {
+     header("Location:secret_question.php?id_user=$iduser&error");
+     exit();
    }
 ?>
