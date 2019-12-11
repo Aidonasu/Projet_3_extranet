6267852a3a2eb908d1af05c22502dd7188bc8b25
Emmanuel Rodrigues <48475342+Aidonasu@users.noreply.github.com>
2019-12-11 23:19:13 +0100

    Integration du thème

--- login/forgot_password.php
+++ login/forgot_password.php
@@ -10,14 +10,16 @@
     <link rel="stylesheet" href="../css/normalize.css">
     <link rel="stylesheet" href="../css/style.css">
   </head>
-  <body>
+  <body class="accueil">
     <header>
       <a href="../index.php"><img src="../logo/GBAF.png" alt="logo1"></a>
     </header>
     <main>
       <form class="" action="valid_forgot_password.php" method="post">
         <?php if (isset($_GET['error'])): ?>
-        <p>Mauvais UserName !</p>
+        <div class="alert alert-dark">
+          <p>Mauvais UserName !</p>
+        </div>
         <?php endif; ?>
         <div class="card border-primary mb-3">
           <div class="card-header">Récupération par nom d'utilisateur</div>
@@ -35,10 +37,11 @@
       </form>
     </main>
     <footer>
-      <ul>
-        <li>Mentions légales</li>
-        <li>Contact</li>
+      <ul class="bg-dark text-white">
+        <li><a href="mentions_legales.php" class="text-white btn btn-outline-primary">Mentions légales</a></li>
+        <li><a href="#" class="text-white btn btn-outline-primary">Contact</a></li>
       </ul>
+    </footer>
     </footer>
   </body>
 </html>
