fd871cca019e5bbcb047f8c2b1767ee6cb3e12e1
Emmanuel Rodrigues <48475342+Aidonasu@users.noreply.github.com>
2019-12-11 19:08:03 +0100

    Ajout beautifier

--- login/parametres.php
+++ login/parametres.php
@@ -1,11 +1,11 @@
 <?php
-	session_start();
+  session_start();
 
-	if (!isset($_SESSION['id_user']) || empty($_SESSION['id_user'])){
-	header('Location:index.php');
-	exit();
-	}
-	else { ?>
+  if (!isset($_SESSION['id_user']) || empty($_SESSION['id_user'])){
+  header('Location:index.php');
+  exit();
+  }
+  else { ?>
 <!DOCTYPE html>
 <html lang="fr">
   <head>
@@ -14,16 +14,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Projet3</title>
     <link rel="stylesheet" href="../fontawesome5/web-fonts-with-css/css/fontawesome-all.min.css">
-		<link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
     <link rel="stylesheet" href="../css/normalize.css">
     <link rel="stylesheet" href="../css/style.css">
-		<script
-				src="https://code.jquery.com/jquery-3.4.1.min.js"
-				integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
-				crossorigin="anonymous"></script>
+    <script
+      src="https://code.jquery.com/jquery-3.4.1.min.js"
+      integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+      crossorigin="anonymous"></script>
   </head>
   <body class="connecter">
-		<header>
+    <header>
       <a href="index.php"><img src="../logo/GBAF.png" alt="logo1"></a>
       <ul class="head_ul alert bg-dark">
         <li class="text-white"><i class="fas fa-user"></i> <?=$_SESSION['nom']?> <?=$_SESSION['prenom'];?></li>
@@ -32,16 +32,15 @@
       </ul>
     </header>
     <main>
-			<form class="" action="valid_modif_parametres.php" method="post">
-				<?php
-        $id = $_GET['account'];
-				require_once '../config-pdo.php';
-				require '../modele/pdoConnect.php';
-				$query = $pdo->prepare('SELECT id_user,nom,prenom,username,password,question,reponse FROM account WHERE id_user=?');
-				$query->execute([$id]);
-				$results = $query->fetch(PDO::FETCH_ASSOC); ?>
-
-				<div class="card border-primary mb-3">
+      <form class="" action="valid_modif_parametres.php" method="post">
+        <?php
+          $id = $_GET['account'];
+          require_once '../config-pdo.php';
+          require '../modele/pdoConnect.php';
+          $query = $pdo->prepare('SELECT id_user,nom,prenom,username,password,question,reponse FROM account WHERE id_user=?');
+          $query->execute([$id]);
+          $results = $query->fetch(PDO::FETCH_ASSOC); ?>
+        <div class="card border-primary mb-3">
           <div class="card-header">Paramètres du compte</div>
           <div class="card-body">
             <p class="card-text">
@@ -52,22 +51,22 @@
               <label for="prenom">Prénom</label>
               <input class="form-input form-control" type="text" name="prenom" value="<?=$results['prenom'];?>" required>
             </p>
-						<p class="card-text">
+            <p class="card-text">
               <label for="username">Username</label>
               <input class="form-input form-control" type="text" name="username" value="<?=$results['username'];?>" required>
             </p>
-						<p class="card-text">
-							<label for="password">Password</label>
-							<input class="form-input form-control" type="password" name="password" id="password" required>
-							<label for="checkbox">
-									<input type="checkbox" id="checkbox">
-									Afficher le mot de passe
-							</label>
-						</p>
+            <p class="card-text">
+              <label for="password">Password</label>
+              <input class="form-input form-control" type="password" name="password" id="password" required>
+              <label for="checkbox">
+              <input type="checkbox" id="checkbox">
+              Afficher le mot de passe
+              </label>
+            </p>
             <div class="form-input form-group">
               <label for="question">Question secrète</label>
               <select class="form-input form-select form-control" name="question" required>
-								<option value="<?=$results['question'];?>" selected>Votre question : <?=$results['question'];?></option>
+                <option value="<?=$results['question'];?>" selected>Votre question : <?=$results['question'];?></option>
                 <option value="">--- Selectionner une question ---</option>
                 <option value="Quel est le nom de mon premier animal domestique ?">Quel est le nom de mon premier animal domestique ?
                 <option value="Quel est le nom du pays que j’aimerais le plus visiter ?">Quel est le nom du pays que j’aimerais le plus visiter ?
@@ -82,28 +81,24 @@
               <label for="reponse">Réponse</label>
               <input class="form-input form-control" type="text" name="reponse" value="<?=$results['reponse'];?>"required>
             </p>
-						<input type="hidden" name="account" value="<?=$id;?>">
-						<ul class="account center">
+            <input type="hidden" name="account" value="<?=$id;?>">
+            <ul class="account center">
               <li><input id="submit" type="submit" class="btn btn-primary" value="Modifier"></li>
               <li><a href="index.php" class="btn btn-link">Annuler</a></li>
             </ul>
-					</div>
-				</div>
-
-
-
-
-			</form>
-			<div class="push"></div>
+          </div>
+        </div>
+      </form>
+      <div class="push"></div>
     </main>
-		<footer>
+    <footer>
       <ul class="bg-dark text-white">
         <li><a href="mentions_legales.php" class="text-white btn btn-outline-primary">Mentions légales</a></li>
         <li><a href="#" class="text-white btn btn-outline-primary">Contact</a></li>
       </ul>
     </footer>
-		<script type="text/javascript" src="../js/reveal_code.js"></script>
+    <script type="text/javascript" src="../js/reveal_code.js"></script>
   </body>
 </html>
 <?php }
-	?>
+  ?>
