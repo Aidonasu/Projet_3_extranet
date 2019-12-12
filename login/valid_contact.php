136a50b4486c3593dce722bad6a3ddab8d143704
Emmanuel Rodrigues <48475342+Aidonasu@users.noreply.github.com>
2019-12-12 16:05:08 +0100

    Ajout valid_contact - login

---
+++ login/valid_contact.php
@@ -0,0 +1,28 @@
+<?php
+  $nom = htmlspecialchars($_POST['nom']);
+  $mail = htmlspecialchars($_POST['mail']);
+  $objet = htmlspecialchars($_POST['objet']);
+  $contenu = htmlspecialchars($_POST['message']);
+
+       // Plusieurs destinataires
+       $to  = 'manu_07130@hotmail.com'; // notez la virgule
+
+       // Sujet
+       $subject = $objet;
+
+       //Contenu
+       $message = $contenu.'<br><br>'.'De '.$nom;
+
+       // Pour envoyer un mail HTML, l'en-tête Content-type doit être défini
+       $headers[] = 'MIME-Version: 1.0';
+       $headers[] = 'Content-type: text/html; charset=iso-8859-1';
+
+       // En-têtes additionnels
+       $headers[] = 'From: '.$mail;
+
+       // Envoi
+       mail($to, $subject,$message, implode("\r\n", $headers));
+
+  header('Location:contact.php');
+  exit();
+  ?>
