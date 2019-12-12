d593ee19e9c92fb5acadb7bea1a3e156e0b0271a
Emmanuel Rodrigues <48475342+Aidonasu@users.noreply.github.com>
2019-12-12 16:02:27 +0100

    Correction lien FOOTER

--- contact.php
+++ contact.php
@@ -51,7 +51,7 @@
     <footer>
       <ul class="bg-dark text-white">
         <li><a href="mentions_legales.php" class="text-white btn btn-outline-primary">Mentions légales</a></li>
-        <li><a href="index.php" class="text-white btn btn-outline-primary">Contact</a></li>
+        <li><a href="contact.php" class="text-white btn btn-outline-primary">Contact</a></li>
       </ul>
     </footer>
     <script type="text/javascript" src="js/reveal_code.js"></script>
