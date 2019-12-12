b9e2207b3cfd310b4c8e8914eaf5c38b1de4bfcc
Emmanuel Rodrigues <48475342+Aidonasu@users.noreply.github.com>
2019-12-12 16:01:25 +0100

    Changement thème du mail

--- valid_contact.php
+++ valid_contact.php
@@ -2,26 +2,26 @@
   $nom = htmlspecialchars($_POST['nom']);
   $mail = htmlspecialchars($_POST['mail']);
   $objet = htmlspecialchars($_POST['objet']);
-  $message = htmlspecialchars($_POST['message']);
+  $contenu = htmlspecialchars($_POST['message']);
 
        // Plusieurs destinataires
        $to  = 'manu_07130@hotmail.com'; // notez la virgule
 
        // Sujet
-       $subject = 'coucou';
+       $subject = $objet;
+
+       //Contenu
+       $message = $contenu.'<br><br>'.'De '.$nom;
 
        // Pour envoyer un mail HTML, l'en-tête Content-type doit être défini
        $headers[] = 'MIME-Version: 1.0';
        $headers[] = 'Content-type: text/html; charset=iso-8859-1';
 
        // En-têtes additionnels
-       $headers[] = 'To: Moi <manu_07130@hotmail.com>';
        $headers[] = 'From: '.$mail;
-       $headers[] = 'Cc: anniversaire_archive@example.com';
-       $headers[] = 'Bcc: anniversaire_verif@example.com';
 
        // Envoi
-       mail($to, $subject, $message, implode("\r\n", $headers));
+       mail($to, $subject,$message, implode("\r\n", $headers));
 
   header('Location:contact.php');
   exit();
