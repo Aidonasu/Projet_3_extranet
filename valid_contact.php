53ea37b56dffbcb4196c96a8320f00ff222e99f8
Aidonas <manu_07130@hotmail.com>
2019-12-13 14:02:52 +0100

    Ajout success - utf8

--- valid_contact.php
+++ valid_contact.php
@@ -15,7 +15,7 @@
 
        // Pour envoyer un mail HTML, l'en-tête Content-type doit être défini
        $headers[] = 'MIME-Version: 1.0';
-       $headers[] = 'Content-type: text/html; charset=iso-8859-1';
+       $headers[] = 'Content-type: text/html; charset=utf8\n';
 
        // En-têtes additionnels
        $headers[] = 'From: '.$mail;
@@ -23,6 +23,6 @@
        // Envoi
        mail($to, $subject,$message, implode("\r\n", $headers));
 
-  header('Location:contact.php');
+  header('Location:contact.php?success');
   exit();
   ?>
