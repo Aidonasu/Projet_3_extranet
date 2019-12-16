17ce9e917eb6b15c03b1610afa3512df377e76e0
Aidonas <manu_07130@hotmail.com>
2019-12-16 15:19:15 +0100

    Ajout connectorcreate

--- login/index.php
+++ login/index.php
@@ -2,7 +2,7 @@
   session_start();
 
   if (!isset($_SESSION['id_user']) || empty($_SESSION['id_user'])){
-  header('Location:../index.php');
+  header('Location:../index.php?connectorcreate');
   exit();
   }
   else { ?>
