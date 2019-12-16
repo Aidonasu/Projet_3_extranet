1c08902bd891df5d800f67070bc0469478d99ae9
Aidonas <manu_07130@hotmail.com>
2019-12-16 15:46:48 +0100

    Ajout error

--- login/valid_modif_parametres.php
+++ login/valid_modif_parametres.php
@@ -13,13 +13,19 @@
   $reponse = htmlspecialchars($_POST['reponse']);
   $id = $_POST['account'];
 
-  require_once '../config-pdo.php';
-  require '../modele/pdoConnect.php';
-  $query = $pdo->prepare('UPDATE account set nom = ?, prenom = ?, username = ?, password = ?, question = ?, reponse = ? WHERE id_user=?');
-  $query->execute([$nom,$prenom,$username,$password,$question,$reponse,$id]);
-
-  header("Location:parametres.php?account=$id");
+  if (empty($nom) || empty($prenom) || empty($username) || empty($password) || empty($question) || empty($reponse)) {
+  sleep(1);
+  header("Location:parametres.php?account=$id&error=2");
   exit();
+  }
+  else {
+      require_once '../config-pdo.php';
+      require '../modele/pdoConnect.php';
+      $query = $pdo->prepare('UPDATE account set nom = ?, prenom = ?, username = ?, password = ?, question = ?, reponse = ? WHERE id_user=?');
+      $query->execute([$nom,$prenom,$username,$password,$question,$reponse,$id]);
 
+      header("Location:parametres.php?account=$id");
+      exit();
 
+  }
    ?>
