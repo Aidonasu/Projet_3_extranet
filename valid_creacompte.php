66aee4fa9c687d5d594b3e0ae558998cad539d2e
Aidonas <manu_07130@hotmail.com>
2019-12-16 15:12:42 +0100

    Ajout error

--- valid_creacompte.php
+++ valid_creacompte.php
@@ -6,16 +6,24 @@
   $question = htmlspecialchars($_POST['question']);
   $reponse = htmlspecialchars($_POST['reponse']);
 
-  require_once 'config-pdo.php';
-  require 'modele/pdoConnect.php';
-  $pdo->exec('SET NAMES UTF8');
-
-  $query = $pdo->prepare("INSERT INTO account (nom, prenom, username, password, question, reponse)
-  VALUES(?,?,?,?,?,?)");
-
-  $query->execute(array($nom,$prenom,$login,$password,$question,$reponse));
-
-
-  header('Location:index.php?success=1');
+  if (empty($login) || empty($password) || empty($nom) || empty($prenom) || empty($question) || empty($reponse)) {
+  sleep(1);
+  header('Location:crea_compte.php?error');
   exit();
-  ?>
+  }
+  else {
+    require_once 'config-pdo.php';
+    require 'modele/pdoConnect.php';
+    $pdo->exec('SET NAMES UTF8');
+
+    $query = $pdo->prepare("INSERT INTO account (nom, prenom, username, password, question, reponse)
+    VALUES(?,?,?,?,?,?)");
+
+    $query->execute(array($nom,$prenom,$login,$password,$question,$reponse));
+
+
+    header('Location:index.php?success=1');
+    exit();
+
+  }
+?>
